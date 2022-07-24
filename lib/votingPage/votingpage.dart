c251a20a4f85ab5e0d0310ae66f8ec87cc99660c
kronusmudpie <cksamonte@gmail.com>
2022-07-23 22:22:01 -0600

    Progress I forgot what I did

--- lib/votingPage/votingpage.dart
+++ lib/votingPage/votingpage.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 import '../classes/Metric.dart';
 import '../classes/MetricsDB.dart';
+import '../styles/metricContainer.dart';
+import '../styles/riskContainer.dart';
 
 class VotingPage extends StatefulWidget {
   @override
@@ -11,8 +14,27 @@ class VotingPage extends StatefulWidget {
 }
 
 class _VotingPageState extends State<VotingPage> {
-  List<Metric>? _myMetrics = MetricsDB().metricDB;
+  List<Metric> _myMetrics = MetricsDB().metricDB!;
+  int _activeIndex = 0;
 
+  // AnimationController? _controller;
+  // Animation<double>? _animation;
+
+  // @override
+  // initState() {
+  //   super.initState();
+  //   _controller = AnimationController(
+  //     duration: const Duration(seconds: 3),
+  //     vsync: this,
+  //   )..repeat(reverse: false);
+  //   _animation = CurvedAnimation(parent: _controller!, curve: Curves.easeIn);
+  // }
+
+  // @override
+  // void dispose() {
+  //   _controller!.dispose();
+  //   super.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -30,13 +52,61 @@ class _VotingPageState extends State<VotingPage> {
               ],
             ),
           ),
-          child: Center(
-            child: Text(
-              "Voting Page",
-              style: TextStyle(
-                fontSize: 100,
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 40),
+                child: CarouselSlider.builder(
+                  itemCount: _myMetrics.length,
+                  options: CarouselOptions(
+                    enlargeCenterPage: true,
+                    enableInfiniteScroll: false,
+                    autoPlay: false,
+                    onPageChanged: (index, reason) {
+                      setState((() => _activeIndex = index));
+                    },
+                  ),
+                  itemBuilder: (context, index, realIndex) {
+                    return buildTitle(
+                      _myMetrics.elementAt(index).title.toString(),
+                    );
+                  },
+                ),
               ),
-            ),
+              Container(
+                child: Column(
+                  children: [
+                    ...(_myMetrics.elementAt(_activeIndex).riskListDesc)
+                        .map((currMetric) {
+                      return RiskContainer(
+                        currMetric,
+                      );
+                    }).toList(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildTitle(String myString) {
+    return Container(
+      width: double.infinity,
+      height: 500,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Center(
+        child: Text(
+          myString,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
           ),
         ),
       ),
